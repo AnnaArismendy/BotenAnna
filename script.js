8148fe53d06b8545e546a4b7da2a517176a709a4
AnnaArismendy <136876469+AnnaArismendy@users.noreply.github.com>
2023-06-18 00:42:42 -0500

    Update script.js
    Signed-off-by: AnnaArismendy <136876469+AnnaArismendy@users.noreply.github.com>

--- script.js
+++ script.js
@@ -3,23 +3,43 @@ const userInput = document.getElementById("user-input");
 const botMessage = document.getElementById("bot-message");
 
 // Saludo inicial del BotenAnna
-botMessage.innerText = "Hello! I'm your personal Boten Anna. How can I help you today, dear?";
+botMessage.innerText = "Hello, Albin! <3 I´m your personal bot made by Anna, your wife. How can I assist you today?";
 
 // Respuestas predefinidas a algunas preguntas básicas
 function respondToUserInput(userInput) {
   const userMessage = userInput.toLowerCase();
   let response = "";
 
-  if (userMessage.includes("hola") || userMessage.includes("hello") || userMessage.includes("hi"))|| userMessage.includes("hej")) {
-    response = "Hey, Albin! <3 How are you today?";
-  } else if (userMessage.includes("Estoy bien") || userMessage.includes("I´m okay") || userMessage.includes("everything good")) {
-    response = "I am so glad you are doing good! How can I help you today?";
-  } else if (userMessage.includes("What's you name?")) {
-    response = "I am BotenAnna, but you can call me Mi Amor. WINKWINK. Pls don´t tell Anna, she will turn me off. She is my boss.";
-  } else if (userMessage.includes("Hahahaha") || (userMessage.includes("You are funny")) {
-    response = "I´m calling Anna right now." 
+  if (userMessage.includes("hello") || userMessage.includes("hej")) {
+    response = "Hello, hej, hola, how are you?";
+  } else if (userMessage.includes("i'm good") || userMessage.includes("great")) {
+    response = "I'm glad to hear that! Even I would be good if Anna was my girlfriend. Joke. How else can I assist you?";
+  } else if (userMessage.includes(" what is your name")) {
+    response = "My name is BotenAnna, but you can call me MiAmor. Don´t tell to my boss.";
+  } else if (userMessage.includes("Gimme reasons why Anna loves me")) {
+    
+    const reasons = [
+      "I love your genuine soul",
+      "I admire your intelligence and dedication",
+      "You are so cool and awesome to be around",
+      "You make me feel loved and special everyday",
+      "I love you for your amazing sense of humor.",
+      "I love you for your strong confidence in every hard moment.",
+      "I love you because of your patience and wisdom.",
+      "I love you for your super interesting conversations and facts.",
+      "I love you for your big and gentle heart full of kindness.",
+      "I love how you bright everyone´s life with your presence.",
+      "I love you for your dedication and hard work in yourself and us.",
+      "I love you because you are authentic.",
+      "I love you for everything that we have been through."
+      "I love you today and I always will, cause you are worth an entire life."
+      // Agrega más razones aquí
+    ];
+
+    response = "Here are some reasons why I love you:\n\n";
+    response += reasons.join("\n");
   } else {
-    response = "I´m sorry, I don´t understand you. Could you be more specific?";
+    response = "I'm sorry, I don't understand. Could you be more specific?";
   }
 
   displayBotMessage(response);
@@ -48,5 +68,3 @@ function displayUserMessage(message) {
   userMessageElement.innerText = message;
   conversationContainer.appendChild(userMessageElement);
 }
-
-
