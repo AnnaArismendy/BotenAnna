a148e7e14f85a11538b0cf11c6f48e54b8b4e830
AnnaArismendy <136876469+AnnaArismendy@users.noreply.github.com>
2023-06-18 01:08:08 -0500

    Update script.js
    Signed-off-by: AnnaArismendy <136876469+AnnaArismendy@users.noreply.github.com>

--- script.js
+++ script.js
@@ -20,17 +20,17 @@ function respondToUserInput(userInput) {
       "I love your genuine soul",
       "I admire your intelligence and dedication",
       "You are so cool and awesome to be around",
-      "You make me feel loved and special everyday",
-      "I love you for your amazing sense of humor.",
-      "I love you for your strong confidence in every hard moment.",
-      "I love you because of your patience and wisdom.",
-      "I love you for your super interesting conversations and facts.",
-      "I love you for your big and gentle heart full of kindness.",
-      "I love how you brighten everyone's life with your presence.",
-      "I love you for your dedication and hard work in yourself and us.",
-      "I love you because you are authentic.",
-      "I love you for everything that we have been through.",
-      "I love you today and I always will, 'cause you are worth an entire life."
+      "You make me feel loved and special every day",
+      "I love you for your amazing sense of humor",
+      "I love you for your strong confidence in every hard moment",
+      "I love you because of your patience and wisdom",
+      "I love you for your super interesting conversations and facts",
+      "I love you for your big and gentle heart full of kindness",
+      "I love how you brighten everyone's life with your presence",
+      "I love you for your dedication and hard work in yourself and us",
+      "I love you because you are authentic",
+      "I love you for everything that we have been through",
+      "I love you today and I always will, because you are worth an entire life",
       // Agrega más razones aquí
     ];
 
