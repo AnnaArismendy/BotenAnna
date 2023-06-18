a54e9f91628248d52f45187617973ffe2eee4af9
AnnaArismendy <136876469+AnnaArismendy@users.noreply.github.com>
2023-06-17 23:32:01 -0500

    Update script.js
    Signed-off-by: AnnaArismendy <136876469+AnnaArismendy@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-function ReasonsWhyILoveYou() {
-    var reasons = [
-        "I love you for your amazing sense of humor.",
-        "I love you for your strong confidence in every hard moment.",
-        "I love you because of your patience and wisdom.",
-        "I love you for your super interesting conversations and facts.",
-        "I love you for your big and gentle heart full of kindness.",
-        "I love how you make me feel loved everyday.",
-        "I love you for your dedication and hard work in yourself and us.",
-        "I love you because you are authentic.",
-        "I love you for everything that we have been through."
-        "I love you today and I always will, cause you are worth an entire life."
-    ];
-
-    var reasonContainer = document.getElementById("reason-text");
-
 const conversationContainer = document.getElementById("conversation");
 const userInput = document.getElementById("user-input");
 const botMessage = document.getElementById("bot-message");
