edc53a8b623bd3871247649b5d9a4d1f700dfc0c
AnnaArismendy <136876469+AnnaArismendy@users.noreply.github.com>
2023-06-17 23:25:05 -0500

    Update script.js
    Signed-off-by: AnnaArismendy <136876469+AnnaArismendy@users.noreply.github.com>

--- script.js
+++ script.js
@@ -13,4 +13,53 @@ function mostrarReasonsWhyILoveYou() {
     ];
 
     var reasonContainer = document.getElementById("reason-text");
-   
+
+    const conversationContainer = document.getElementById("conversation");
+const userInput = document.getElementById("user-input");
+const botMessage = document.getElementById("bot-message");
+
+// Saludo inicial del BotenAnna
+botMessage.innerText = "¡Hola, Hej! I am Boten Anna. How can I help you today?";
+
+// Respuestas predefinidas a algunas preguntas básicas
+function respondToUserInput(userInput) {
+  const userMessage = userInput.toLowerCase();
+  let response = "";
+
+  if (userMessage.includes("Hello, Hej") || userMessage.includes("greetings")) {
+    response = "¡Hola! <3 ¿Cómo estás, querido Albin?";
+  } else if (userMessage.includes("I'm okay") || userMessage.includes("Estoy bien")) {
+    response = "I´m so glad you are okay. How can I help you today?";
+  } else if (userMessage.includes("Who are you?")) {
+    response = "My name is Boten Anna. But you can call me Mi Amor. Winkwink.";
+  } else {
+    response = "I´m sorry, I can´t understand you. Could you be more specific, please?";
+  }
+
+  displayBotMessage(response);
+}
+
+// Mostrar mensaje del BotenAnna en la conversación
+function displayBotMessage(message) {
+  const botMessageElement = document.createElement("p");
+  botMessageElement.innerText = message;
+  conversationContainer.appendChild(botMessageElement);
+}
+
+// Manejar el envío del usuario cuando presiona Enter
+userInput.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    const userMessage = userInput.value;
+    displayUserMessage(userMessage);
+    respondToUserInput(userMessage);
+    userInput.value = "";
+  }
+});
+
+// Mostrar mensaje del usuario en la conversación
+function displayUserMessage(message) {
+  const userMessageElement = document.createElement("p");
+  userMessageElement.innerText = message;
+  conversationContainer.appendChild(userMessageElement);
+}
+
