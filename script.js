21b30c4dc55c2317d68312c71e895877d32dd967
AnnaArismendy <136876469+AnnaArismendy@users.noreply.github.com>
2023-06-18 00:26:27 -0500

    Update script.js
    Signed-off-by: AnnaArismendy <136876469+AnnaArismendy@users.noreply.github.com>

--- script.js
+++ script.js
@@ -3,21 +3,23 @@ const userInput = document.getElementById("user-input");
 const botMessage = document.getElementById("bot-message");
 
 // Saludo inicial del BotenAnna
-botMessage.innerText = "¡Hola! Soy tu BotenAnna. ¿En qué puedo ayudarte hoy?";
+botMessage.innerText = "Hello! I'm your personal Boten Anna. How can I help you today, dear?";
 
 // Respuestas predefinidas a algunas preguntas básicas
 function respondToUserInput(userInput) {
   const userMessage = userInput.toLowerCase();
   let response = "";
 
-  if (userMessage.includes("hola") || userMessage.includes("saludos")) {
-    response = "¡Hola! ¿Cómo estás?";
-  } else if (userMessage.includes("bien") || userMessage.includes("genial")) {
-    response = "Me alegra escuchar eso. ¿En qué más puedo ayudarte?";
-  } else if (userMessage.includes("nombre")) {
-    response = "Mi nombre es BotenAnna, pero puedes llamarme como quieras.";
+  if (userMessage.includes("hola") || userMessage.includes("hello") || userMessage.includes("hi"))|| userMessage.includes("hej")) {
+    response = "Hey, Albin! <3 How are you today?";
+  } else if (userMessage.includes("Estoy bien") || userMessage.includes("I´m okay") || userMessage.includes("everything good")) {
+    response = "I am so glad you are doing good! How can I help you today?";
+  } else if (userMessage.includes("What's you name?")) {
+    response = "I am BotenAnna, but you can call me Mi Amor. WINKWINK. Pls don´t tell Anna, she will turn me off. She is my boss.";
+  } else if (userMessage.includes("Hahahaha") || (userMessage.includes("You are funny")) {
+    response = "I´m calling Anna right now." 
   } else {
-    response = "Lo siento, no entiendo. ¿Podrías ser más específico/a?";
+    response = "I´m sorry, I don´t understand you. Could you be more specific?";
   }
 
   displayBotMessage(response);
