71ebc7fa1b2f95536d3fe6d5f7390226924fcd1b
AnnaArismendy <136876469+AnnaArismendy@users.noreply.github.com>
2023-06-18 01:17:41 -0500

    Update script.js
    Signed-off-by: AnnaArismendy <136876469+AnnaArismendy@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,70 +1,90 @@
-const conversationContainer = document.getElementById("conversation");
-const userInput = document.getElementById("user-input");
+<!DOCTYPE html>
+<html>
+<head>
+  <title>BotenAnna</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+  <h1>BotenAnna</h1>
+  <div id="conversation"></div>
+  <input type="text" id="user-input" placeholder="Tell me something..." autofocus>
 
-// Saludo inicial del BotenAnna
-displayBotMessage("Hello, Albin! <3 I'm your personal bot made by Anna, your wife. How can I assist you today?");
+  <script>
+    const conversationContainer = document.getElementById("conversation");
+    const userInput = document.getElementById("user-input");
+    const botMessage = document.getElementById("bot-message");
 
-// Respuestas predefinidas a algunas preguntas básicas
-function respondToUserInput(userInput) {
-  const userMessage = userInput.toLowerCase();
-  let response = "";
+    // Saludo inicial del BotenAnna
+    botMessage.innerText = "Hello, Albin! <3 I´m your personal bot made by Anna, your wife. How can I assist you today?";
 
-  if (userMessage.includes("hello") || userMessage.includes("hej")) {
-    response = "Hello, hej, hola, how are you?";
-  } else if (userMessage.includes("i'm good") || userMessage.includes("great")) {
-    response = "I'm glad to hear that! Even I would be good if Anna was my girlfriend. Joke. How else can I assist you?";
-  } else if (userMessage.includes("what is your name")) {
-    response = "My name is BotenAnna, but you can call me MiAmor. Don't tell my boss.";
-  } else if (userMessage.includes("reasons why Anna loves me")) {
+    // Respuestas predefinidas a algunas preguntas básicas
+    function respondToUserInput(userInput) {
+      const userMessage = userInput.toLowerCase();
+      console.log("User message:", userMessage);
 
-    const reasons = [
-      "I love your genuine soul",
-      "I admire your intelligence and dedication",
-      "You are so cool and awesome to be around",
-      "You make me feel loved and special every day",
-      "I love you for your amazing sense of humor",
-      "I love you for your strong confidence in every hard moment",
-      "I love you because of your patience and wisdom",
-      "I love you for your super interesting conversations and facts",
-      "I love you for your big and gentle heart full of kindness",
-      "I love how you brighten everyone's life with your presence",
-      "I love you for your dedication and hard work in yourself and us",
-      "I love you because you are authentic",
-      "I love you for everything that we have been through",
-      "I love you today and I always will, because you are worth an entire life",
-      // Agrega más razones aquí
-    ];
+      let response = "";
 
-    response = "Here are some reasons why Anna loves you:\n\n";
-    response += reasons.join("\n");
-  } else {
-    response = "I'm sorry, I don't understand. Could you be more specific?";
-  }
+      if (userMessage.includes("hello") || userMessage.includes("hej")) {
+        response = "Hello, hej, hola, how are you?";
+      } else if (userMessage.includes("i'm good") || userMessage.includes("great")) {
+        response = "I'm glad to hear that! Even I would be good if Anna was my girlfriend. Joke. How else can I assist you?";
+      } else if (userMessage.includes("what is your name")) {
+        response = "My name is BotenAnna, but you can call me MiAmor. Don´t tell to my boss.";
+      } else if (userMessage.includes("gimme reasons why Anna loves me")) {
+        const reasons = [
+          "I love your genuine soul",
+          "I admire your intelligence and dedication",
+          "You are so cool and awesome to be around",
+          "You make me feel loved and special everyday",
+          "I love you for your amazing sense of humor.",
+          "I love you for your strong confidence in every hard moment.",
+          "I love you because of your patience and wisdom.",
+          "I love you for your super interesting conversations and facts.",
+          "I love you for your big and gentle heart full of kindness.",
+          "I love how you bright everyone´s life with your presence.",
+          "I love you for your dedication and hard work in yourself and us.",
+          "I love you because you are authentic.",
+          "I love you for everything that we have been through.",
+          "I love you today and I always will, cause you are worth an entire life.",
+          // Agrega más razones aquí
+        ];
 
-  displayBotMessage(response);
-}
+        response = "Here are some reasons why I love you:\n\n";
+        response += reasons.join("\n");
+      } else {
+        response = "I'm sorry, I don't understand. Could you be more specific?";
+      }
 
-// Mostrar mensaje del BotenAnna en la conversación
-function displayBotMessage(message) {
-  const botMessageElement = document.createElement("p");
-  botMessageElement.innerText = message;
-  conversationContainer.appendChild(botMessageElement);
-}
+      console.log("BotenAnna response:", response);
+      displayBotMessage(response);
+    }
 
-// Manejar el envío del usuario cuando presiona Enter
-userInput.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    const userMessage = userInput.value;
-    displayUserMessage(userMessage);
-    respondToUserInput(userMessage);
-    userInput.value = "";
-  }
-});
+    // Mostrar mensaje del BotenAnna en la conversación
+    function displayBotMessage(message) {
+      const botMessageElement = document.createElement("p");
+      botMessageElement.innerText = message;
+      conversationContainer.appendChild(botMessageElement);
+    }
 
-// Mostrar mensaje del usuario en la conversación
-function displayUserMessage(message) {
-  const userMessageElement = document.createElement("p");
-  userMessageElement.innerText = message;
-  conversationContainer.appendChild(userMessageElement);
-}
+    // Manejar el envío del usuario cuando presiona Enter
+    userInput.addEventListener("keyup", function(event) {
+      if (event.keyCode === 13) {
+        const userMessage = userInput.value;
+        console.log("User input:", userMessage);
+        displayUserMessage(userMessage);
+        respondToUserInput(userMessage);
+        userInput.value = "";
+      }
+    });
+
+    // Mostrar mensaje del usuario en la conversación
+    function displayUserMessage(message) {
+      const userMessageElement = document.createElement("p");
+      userMessageElement.innerText = message;
+      conversationContainer.appendChild(userMessageElement);
+    }
+  </script>
+</body>
+</html>
+
 
